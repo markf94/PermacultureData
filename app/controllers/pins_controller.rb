d95580226d6feaef2b337495a0672d5aac87cf84
markf94 <markfingerhuth@protonmail.com>
2016-10-21 00:40:09 +0200

    added sign up with devise. user/pin association

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -7,13 +7,17 @@ class PinsController < ApplicationController
   end
 
   def create
-    @pin = Pin.new(pin_params)
+    @pin = current_user.pins.build(pin_params)
 
     if @pin.save
       redirect_to @pin, notice: "Successfully created new Pin"
     else
       render 'new'
     end
+  end
+
+  def new
+    @pin = current_user.pins.build
   end
 
   def edit
@@ -31,7 +35,6 @@ class PinsController < ApplicationController
     @pin.destroy
     redirect_to root_path
   end
-
 
   def show
   end

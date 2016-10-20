112118c66961dbc1e542e75d9c796ec839d24e1a
markf94 <markfingerhuth@protonmail.com>
2016-10-21 00:18:48 +0200

    initial commit

---
+++ app/controllers/pins_controller.rb
@@ -0,0 +1,49 @@
+class PinsController < ApplicationController
+
+  before_action :find_pin, only: [:show, :edit, :update, :destroy]
+
+  def index #holds all the pins in reverse chronological order
+    @pins = Pin.all.order("created_at DESC")
+  end
+
+  def create
+    @pin = Pin.new(pin_params)
+
+    if @pin.save
+      redirect_to @pin, notice: "Successfully created new Pin"
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @pin.update(pin_params)
+      redirect_to @pin, notice: "Pin was successfully updated"
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @pin.destroy
+    redirect_to root_path
+  end
+
+
+  def show
+  end
+
+  private # private method below
+
+  def pin_params #define pin parameters and permit only title and description
+    params.require(:pin).permit(:title, :description)
+  end
+
+  def find_pin
+    @pin = Pin.find(params[:id])
+  end
+
+end
